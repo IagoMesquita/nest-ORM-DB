90c245d1e345be25581270c1327bb9ea97099081
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-09-02 17:47:25 -0300

    feat: config database with typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [CoursesModule],
+  imports: [CoursesModule, TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'mysql',
+    autoLoadEntities: true,
+    synchronize: true
+  })],
   controllers: [AppController],
   providers: [AppService],
-})
+}) 
 export class AppModule {}
