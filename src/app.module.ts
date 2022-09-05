361fd5eef1f29a574e820109689cc1a4fdbe06c9
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-09-05 18:00:23 -0300

    refactor: comment service and controller default

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,26 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+
 import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [CoursesModule, TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'root',
-    database: 'mysql',
-    autoLoadEntities: true,
-    synchronize: true
-  })],
-  controllers: [AppController],
-  providers: [AppService],
-}) 
+  imports: [
+    CoursesModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'mysql',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+  // controllers: [AppController],
+  // providers: [AppService],
+})
 export class AppModule {}
