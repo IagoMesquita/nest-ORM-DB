64b2ef849e249f60ef5ae428108543fc7fa44181
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-11-21 21:23:29 -0300

    chore: script sh

---
+++ .docker/entrypoin.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+npm install
+npm run build
+npx typeorm migrations:run
+npm run start:dev
+
+# quando subimos um conaiter como o nosso, com uma imagem alpine
+# para rodar comandos automaticamente quando o container for criado, criamos um spript.
+# E nos containers existe um script chamado "entryoint" que é o primeiro recurso que entra em
+# execução, depois que todo sistma linux é carregar.
+#na linha 1 dizemos que estamos usando o bash, que é o que instalamos.
+# o buil é por cona das migrations, elas executam o arquivo geredo pelo build
